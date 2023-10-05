668aba02990fb2fe226494f5f3a95d6d1c2c2ef7
ymkim97 <rladudaud97@naver.com>
2023-10-05 21:54:07 +0900

    Querydsl

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,7 +17,8 @@ import org.springframework.context.annotation.Profile;
 // @Import(JdbcTemplateV3Config.class)
 // @Import(MyBatisConfig.class)
 // @Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+// @Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
